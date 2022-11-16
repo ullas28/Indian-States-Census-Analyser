15501a758589e556d7e99d080a30f7745f56853b
unknown <you@example.com>
2022-11-16 20:33:44 +0530

    Added : wrong header csv file

--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -10,6 +10,8 @@ class CensusAnalyserTest {
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String INCORRECT_FILE_FORMAT = "C:\\Users\\ULLASKUMAR K\\Desktop\\Prophecy.txt";
     private static final String CSV_WITH_WRONG_DELIMITER = "C:\\Users\\ULLASKUMAR K\\Desktop\\Data.csv";
+
+    private static final String CSV_WITH_INCORRECT_HEADER = "C:\\Users\\ULLASKUMAR K\\Desktop\\Data.csv";
     @Test
     public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecordsHappyTest() throws CensusAnalyzerException {
         CensusAnalyser censusAnalyser = new CensusAnalyser();
@@ -38,10 +40,19 @@ class CensusAnalyserTest {
         }
 
     @Test
-    public void givenIndianCensusCSVFile_WhenCustomDelimiter_ShouldThrowException() {
+    public void givenIndianCensusCSVFile_WhenCustomDelimiter_ShouldThrowExceptionSadTest(){
         CensusAnalyser censusAnalyser = new CensusAnalyser();
         CensusAnalyzerException exceptionRule = Assertions.assertThrows(CensusAnalyzerException.class,()->{
             censusAnalyser.loadIndiaCensusData(CSV_WITH_WRONG_DELIMITER);
+        });
+        Assertions.assertEquals(CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER, exceptionRule.type);
+    }
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenIncorrectHeader_ShouldThrowExceptionSadTest() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        CensusAnalyzerException exceptionRule = Assertions.assertThrows(CensusAnalyzerException.class,()->{
+            censusAnalyser.loadIndiaCensusData(CSV_WITH_INCORRECT_HEADER);
         });
         Assertions.assertEquals(CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER, exceptionRule.type);
     }
