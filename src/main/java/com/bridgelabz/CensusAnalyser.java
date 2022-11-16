89100aacd6f7acbbcd0f2b0dff783083f6e44bd4
unknown <you@example.com>
2022-11-16 19:38:15 +0530

    Added : test case for unsupported file format

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -10,25 +11,44 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class CensusAnalyser {
+
+    public boolean isValidFile (String filePath, String desriedType){
+        File fileToCheck = new File(filePath);
+        String filename = fileToCheck.getName();
+        String extension = "";
+        int i = filename.lastIndexOf('.');
+        if (i > 0) {
+            extension = filename.substring(i + 1);
+        }
+        return extension.equals(desriedType);
+    }
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyzerException {
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(IndiaCensusCSV.class);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
-            int namOfEateries = 0;
-            while (censusCSVIterator.hasNext()) {
-                namOfEateries++;
-                IndiaCensusCSV censusData = censusCSVIterator.next();
+        boolean b = isValidFile(csvFilePath, "csv");
+        if (b){
+            try {
+                Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+                CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+                csvToBeanBuilder.withType(IndiaCensusCSV.class);
+                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+                CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+                Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+                ;
+                int namOfEateries = 0;
+                while (censusCSVIterator.hasNext()) {
+                    namOfEateries++;
+                    IndiaCensusCSV censusData = censusCSVIterator.next();
+                }
+                return namOfEateries;
             }
-            return namOfEateries;
-        } catch (IOException e) {
-            throw new CensusAnalyzerException(e.getMessage(), CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM);
-        }
-        catch(RuntimeException e) {
-            throw new CensusAnalyzerException("CSV File Must Have Comma As Delimiter Or Has Incorrect Header", CensusAnalyzerException.ExceptionType.CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER);
-        }
+            catch (IOException e) {
+                throw new CensusAnalyzerException(e.getMessage(), CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM);
+            } catch (RuntimeException e) {
+                throw new CensusAnalyzerException("CSV File Must Have Comma As Delimiter Or Has Incorrect Header", CensusAnalyzerException.ExceptionType.CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER);
+            } }
+            else {
+                throw new CensusAnalyzerException("File format not supported", CensusAnalyzerException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT);
+            }
     }
 }
+
+
--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static com.bridgelabz.CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM;
+import static com.bridgelabz.CensusAnalyzerException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT;
 
 class CensusAnalyserTest {
     private String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\ULLASKUMAR K\\Desktop\\CensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
-
+    private static final String INCORRECT_FILE_FORMAT = "C:\\Users\\ULLASKUMAR K\\Desktop\\Prophecy.txt";
     @Test
     public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecordsHappyTest() throws CensusAnalyzerException {
         CensusAnalyser censusAnalyser = new CensusAnalyser();
@@ -25,4 +26,14 @@ class CensusAnalyserTest {
             });
             Assertions.assertEquals(CENSUS_FILE_PROBLEM, exceptionRule.type);
     }
-}
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenCorrectPathButWrongFileFormat_ShouldThrowExceptionSadTest() {
+
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            CensusAnalyzerException exceptionRule = Assertions.assertThrows(CensusAnalyzerException.class,()->{
+                censusAnalyser.loadIndiaCensusData(INCORRECT_FILE_FORMAT);
+            });
+            Assertions.assertEquals(CENSUS_INCORRECT_FILE_FORMAT, exceptionRule.type);
+        }
+    }
