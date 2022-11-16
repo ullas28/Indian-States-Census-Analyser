ff655f81eb600e7e707e229c2e6af2a3c401ed8e
unknown <you@example.com>
2022-11-16 19:59:01 +0530

    Added : Incorrect delimitor

--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -3,13 +3,13 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static com.bridgelabz.CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM;
-import static com.bridgelabz.CensusAnalyzerException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT;
+import static com.bridgelabz.CensusAnalyzerException.ExceptionType.*;
 
 class CensusAnalyserTest {
     private String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\ULLASKUMAR K\\Desktop\\CensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String INCORRECT_FILE_FORMAT = "C:\\Users\\ULLASKUMAR K\\Desktop\\Prophecy.txt";
+    private static final String CSV_WITH_WRONG_DELIMITER = "C:\\Users\\ULLASKUMAR K\\Desktop\\Data.csv";
     @Test
     public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecordsHappyTest() throws CensusAnalyzerException {
         CensusAnalyser censusAnalyser = new CensusAnalyser();
@@ -36,4 +36,13 @@ class CensusAnalyserTest {
             });
             Assertions.assertEquals(CENSUS_INCORRECT_FILE_FORMAT, exceptionRule.type);
         }
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenCustomDelimiter_ShouldThrowException() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        CensusAnalyzerException exceptionRule = Assertions.assertThrows(CensusAnalyzerException.class,()->{
+            censusAnalyser.loadIndiaCensusData(CSV_WITH_WRONG_DELIMITER);
+        });
+        Assertions.assertEquals(CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER, exceptionRule.type);
+    }
     }
